3f37e21f5f997007a904577a54c169563a844d5b
justindstein <jaydstein@gmail.com>
2023-09-13 15:23:59 -0300

    Working timer animation with introduction of eventing definitions.

--- Assets/Scripts/Controllers/TimerController.cs
+++ Assets/Scripts/Controllers/TimerController.cs
@@ -11,22 +11,43 @@ public class TimerController : MonoBehaviour
 
     public float TimeToAnswer;
 
-    private float totalElapsedTime;
+    private float maxFillAmount;
+
+    private float elapsedTime;
+
+    private bool isTimerActive;
 
     void Start()
     {
-        this.totalElapsedTime = 0;
-    }
-
-    void Update()
-    {
-        this.totalElapsedTime += Time.deltaTime;
-        Debug.Log(string.Format("Setting [fillAmount: {0}]", (1 - (this.TimeToAnswer / Mathf.Min(this.totalElapsedTime, this.TimeToAnswer)))));
-        this.ParentImage.fillAmount = (1 - (this.TimeToAnswer / Mathf.Min(this.totalElapsedTime, this.TimeToAnswer)));
+        this.maxFillAmount = 1;
+        this.elapsedTime = 0;
+        this.isTimerActive = false;
     }
 
     void FixedUpdate()
     {
+        this.elapsedTime += Time.deltaTime;
+        this.UpdateTimerImage(this.ParentImage, this.maxFillAmount, this.elapsedTime, this.TimeToAnswer);
+    }
 
+    public void StartTimer()
+    {
+        this.isTimerActive = true;
+    }
+
+    public void StopTimer()
+    {
+        this.isTimerActive = false;
+        this.elapsedTime = 0;
+    }
+
+    private void UpdateTimerImage(Image image, float maxFillAmount, float elapsedTime, float timeToAnswer)
+    {
+        float total = Mathf.Max(maxFillAmount - (elapsedTime / TimeToAnswer), 0);
+        this.ParentImage.fillAmount = total;
+
+#if UNITY_EDITOR
+        Debug.Log(string.Format("{0}: Mathf.Max({1} - ({2} / {3}), 0)", total, maxFillAmount, elapsedTime, timeToAnswer));
+#endif
     }
 }
