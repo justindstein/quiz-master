1f2403c813a98bd0db42ee18f51b9ddb590e09b7
justindstein <jaydstein@gmail.com>
2023-09-16 18:18:03 -0300

    New question loads after timer expires.

--- Assets/Scripts/Controllers/TimerController.cs
+++ Assets/Scripts/Controllers/TimerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class TimerController : MonoBehaviour
@@ -9,45 +10,54 @@ public class TimerController : MonoBehaviour
 
     public Sprite PausedSprite;
 
-    public float TimeToAnswer;
-
     private float maxFillAmount = 1;
 
     private float elapsedTime;
 
-    private bool isTimerActive;
+    public GameEvent OnTimerExpired;
+
+    private bool timerActive;
+
+    public UnityEvent TimerExpired;
+
+    public FloatVariable QuestionDuration;
 
     void FixedUpdate()
     {
-        if (isTimerActive)
+        if (this.timerActive)
         {
             this.elapsedTime += Time.deltaTime;
-            this.UpdateTimerImage(this.ParentImage, this.maxFillAmount, this.elapsedTime, this.TimeToAnswer);
+            this.UpdateTimerImage(this.ParentImage, this.maxFillAmount, this.elapsedTime, this.QuestionDuration.Value);
+
+            if(this.elapsedTime > this.QuestionDuration.Value)
+            {
+                this.TimerExpired.Invoke();
+                this.timerActive = false;
+            }
         }
     }
 
     public void StartTimer()
     {
         this.elapsedTime = 0;
-        this.isTimerActive = true;
-        this.UpdateTimerImage(this.ParentImage, this.maxFillAmount, this.elapsedTime, this.TimeToAnswer);
+        this.timerActive = true;
+        this.UpdateTimerImage(this.ParentImage, this.maxFillAmount, this.elapsedTime, this.QuestionDuration.Value);
         this.ParentImage.sprite = this.DefaultSprite;
     }
 
     public void StopTimer()
     {
-        this.isTimerActive = false;
+        this.timerActive = false;
         this.ParentImage.sprite = this.PausedSprite;
     }
 
     private void UpdateTimerImage(Image image, float maxFillAmount, float elapsedTime, float timeToAnswer)
     {
-        float total = Mathf.Max(maxFillAmount - (elapsedTime / TimeToAnswer), 0);
+        float total = Mathf.Max(maxFillAmount - (elapsedTime / this.QuestionDuration.Value), 0);
         this.ParentImage.fillAmount = total;
 
     #if UNITY_EDITOR
-        //Debug.Log(string.Format("{0}: Mathf.Max({1} - ({2} / {3}), 0)", total, maxFillAmount, elapsedTime, timeToAnswer));
+        Debug.Log(string.Format("{0}: Mathf.Max({1} - ({2} / {3}), 0)", total, maxFillAmount, elapsedTime, timeToAnswer));
     #endif
     }
 }
-
